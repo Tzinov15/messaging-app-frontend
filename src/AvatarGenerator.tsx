c80ef45b64d06acc34821da770e34217664ea725
Alex <alex.tzinov@gmail.com>
2019-12-06 10:51:34 -0700

    Update src/AvatarGenerator.tsx

--- src/AvatarGenerator.tsx
+++ src/AvatarGenerator.tsx
@@ -185,5 +185,5 @@ export const RandomAvatar = (props: typeof RandomAvatarOptions) => (
 );
 
 export const RandomAvatarSmall = (props: typeof RandomAvatarOptions) => (
-  <Avatar {...props} style={{ width: "35", height: "35" }} />
+  <Avatar {...props} style={{ width: "35px", height: "35px" }} />
 );
